e7353f871d602f0397b44c91b0d687b3b7d15b4b
dalila <dalilaacunamiglio788@gmail.com>
2026-03-19 03:29:57 -0300

    correccion de la rutas posts

--- src/config.js
+++ src/config.js
@@ -1,18 +1,8 @@
-import fs from 'fs';
-import process from 'node:process';
 import pg from 'pg';
 
-// Solo carga el .env si estás en local (desarrollo)
-if (fs.existsSync('.env')) {
-  process.loadEnvFile('.env');
-}
-
 export const pool = new pg.Pool({
-  user: process.env.DBUSER,
-  host: process.env.DBHOST,
-  database: process.env.DBDATABASE,
-  password: process.env.DBPASSWORD,
-  port: process.env.DBPORT,
-  // CONFIGURACIÓN PARA RAILWAY (IMPORTANTE)
-  ssl: process.env.DBHOST !== 'localhost' ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
